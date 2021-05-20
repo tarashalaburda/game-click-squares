763c92c0c9fadb3a65650e22dce922ab2f19be3e
Taras Halaburda <tarashalaburda@gmail.com>
2021-05-20 12:45:59 +0300

    creating a square

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-let $start = document.querySelector('#start')
-let $game = document.querySelector('#game')
+let $start = document.querySelector('#start');
+let $game = document.querySelector('#game');
 
-$start.addEventListener('click', startGame)
+let score = 0;
+
+$start.addEventListener('click', startGame);
+$game.addEventListener('click', handleBoxClick)
 
 function startGame() {
   // console.log('Start');
-  $game.style.backgroundColor = '#fff'
-  $start.classList.add('hide')
+  $game.style.backgroundColor = '#fff';
+  $start.classList.add('hide');
 
-  renderBox()
+  renderBox();
+}
+
+function handleBoxClick(event) {
+  // console.log(event.target.dataset);
+  if (event.target.dataset.box) {
+    score++;
+    renderBox();
+  }
 }
 
 function renderBox() {
+  $game.innerHTML = '';
+  let box = document.createElement('div');
+
+  box.style.height = box.style.width = '50px';
+  box.style.position = 'absolute';
+  box.style.backgroundColor = '#000';
+  box.style.top = '50px';
+  box.style.left = '70px';
+  box.style.cursor = 'pointer';
+  box.setAttribute('data-box', 'true');
+
+  $game.insertAdjacentElement('afterbegin', box)
+
 
 }
